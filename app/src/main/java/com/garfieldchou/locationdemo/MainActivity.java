948b8bf994ba7ccd5c4e3ef0077c06e8fae62d2a
shaudai <shaudai0402@hotmail.com>
2017-01-10 07:33:44 +0800

    Check if this app has permission or not. If not, request it
    ContextCompact.
    .checkSelfPermission()
    
    ActivityCompact
    .requestPermissions()

--- app/src/main/java/com/garfieldchou/locationdemo/MainActivity.java
+++ app/src/main/java/com/garfieldchou/locationdemo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.garfieldchou.locationdemo;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // ask for it
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        }
 
     }
 }
